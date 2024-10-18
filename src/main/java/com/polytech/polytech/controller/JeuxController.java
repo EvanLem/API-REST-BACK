9544648d25e3b2ad1a2a5581c4986bc347cd25f0
meehdii18 <mehdiaml@icloud.com>
2024-10-18 15:57:26 +0200

    WIP Service

--- src/main/java/com/polytech/polytech/controller/JeuxController.java
+++ src/main/java/com/polytech/polytech/controller/JeuxController.java
@@ -1,4 +1,50 @@
 package com.polytech.polytech.controller;
 
+import com.polytech.polytech.dto.JeuxDTO;
+import com.polytech.polytech.service.JeuxService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/jeux")
 public class JeuxController {
+
+    @Autowired
+    private JeuxService jeuxService;
+
+    // CRUD (read all / read by id)
+
+    @PostMapping
+    public ResponseEntity<JeuxDTO> createJeux(@RequestBody JeuxDTO jeuxDTO) {
+        JeuxDTO createdJeux = jeuxService.createJeux(jeuxDTO);
+        return ResponseEntity.ok(createdJeux);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JeuxDTO> getJeuxById(@PathVariable Integer id) {
+        JeuxDTO jeuxDTO = jeuxService.getJeuxById(id);
+        return ResponseEntity.ok(jeuxDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<JeuxDTO>> getAllJeux() {
+        List<JeuxDTO> jeuxList = jeuxService.getAllJeux();
+        return ResponseEntity.ok(jeuxList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<JeuxDTO> updateJeux(@PathVariable Integer id, @RequestBody JeuxDTO jeuxDTO) {
+        JeuxDTO updatedJeux = jeuxService.updateJeux(id, jeuxDTO);
+        return ResponseEntity.ok(updatedJeux);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteJeux(@PathVariable Integer id) {
+        jeuxService.deleteJeux(id);
+        return ResponseEntity.noContent().build();
+    }
 }
