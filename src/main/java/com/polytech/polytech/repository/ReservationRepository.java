a3deeb484da421cb241f723723f710275a866c79
meehdii18 <mehdiaml@icloud.com>
2024-11-22 14:39:52 +0100

    Update CORS config

--- src/main/java/com/polytech/polytech/repository/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.polytech.polytech.repository;
-
-import com.polytech.polytech.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReservationRepository extends JpaRepository<Reservation,Integer>{
-}
