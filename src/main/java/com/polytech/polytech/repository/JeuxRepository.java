14502857d06fad60f5dbdaf8f75842bbab4cb89b
homerduc <alexandre.duc00@orange.fr>
2024-11-22 14:40:09 +0100

    CLeanup (suppression de mon ancien projet)

--- src/main/java/com/polytech/polytech/repository/JeuxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.polytech.polytech.repository;
-
-import com.polytech.polytech.entity.Jeux;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JeuxRepository extends JpaRepository<Jeux, Integer> {
-}
