ea79b866c980920a4bc56f7b7cb306a8c961e333
EvanLem <165893283+EvanLem@users.noreply.github.com>
2024-11-29 15:12:08 +0100

    Exception dans les controller

--- src/main/java/com/example/projet/controller/GameController.java
+++ src/main/java/com/example/projet/controller/GameController.java
@@ -1,13 +1,16 @@
 package com.example.projet.controller;
 
 import com.example.projet.DTO.GameDTO;
+import com.example.projet.exception.ApiError;
 import com.example.projet.service.GameService;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/api/game")
@@ -18,30 +21,130 @@ public class GameController {
 
     // Endpoint pour récupérer tous les jeux
     @GetMapping(produces="application/json")
-    public ResponseEntity<List<GameDTO>> getGames() {
-        List<GameDTO> gameList = gameService.getGames();
-        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    public ResponseEntity<?> getGames() {
+        try {
+            return new ResponseEntity<>(gameService.getGames(), HttpStatus.OK);
+        }catch (RuntimeException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     @GetMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<GameDTO> getGamesById(@PathVariable("id") Integer id) {
-        GameDTO gameDTO = gameService.getGameById(id);
-        return new ResponseEntity<>(gameDTO, HttpStatus.OK);
+    public ResponseEntity<?> getGamesById(@PathVariable("id") Integer id) {
+        try {
+            return new ResponseEntity<>(gameService.getGameById(id), HttpStatus.OK);
+        }catch (RuntimeException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // Endpoint pour ajouter un jeu
     @PostMapping()
-    public ResponseEntity<GameDTO> addGame(@RequestBody GameDTO gameDTO) {
-        return new ResponseEntity<>(gameService.addGame(gameDTO), HttpStatus.OK);
+    public ResponseEntity<?> addGame(@RequestBody GameDTO gameDTO) {
+        try {
+            return new ResponseEntity<>(gameService.addGame(gameDTO), HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteGame(@PathVariable("id") Integer id) {
-        gameService.deleteGame(id);
+    public ResponseEntity<ApiError> deleteGame(@PathVariable("id") Integer id) {
+        try {
+            gameService.deleteGame(id);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return null;
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<GameDTO> updateGame(@PathVariable("id") Integer id, @RequestBody GameDTO gameDTO) {
-        return new ResponseEntity<>(gameService.updateGame(id, gameDTO), HttpStatus.OK);
+    public ResponseEntity<?> updateGame(@PathVariable("id") Integer id, @RequestBody GameDTO gameDTO) {
+        try {
+            return new ResponseEntity<>(gameService.updateGame(id, gameDTO), HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- src/main/java/com/example/projet/controller/UserController.java
+++ src/main/java/com/example/projet/controller/UserController.java
@@ -3,12 +3,16 @@ package com.example.projet.controller;
 import com.example.projet.DTO.GameDTO;
 import com.example.projet.DTO.UserDTO;
 import com.example.projet.component.UserMapper;
+import com.example.projet.exception.ApiError;
 import com.example.projet.service.UserService;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -24,23 +28,114 @@ public class UserController {
     private UserMapper mapper;
 
     @GetMapping(produces="application/json")
-    public ResponseEntity<List<UserDTO>> getUsers() {
-        return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+    public ResponseEntity<?> getUsers() {
+        try {
+            return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping(value="/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable("id") Integer id) {
-        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    public ResponseEntity<?> getUserById(@PathVariable("id") Integer id) {
+        try {
+            return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping()
-    public ResponseEntity<UserDTO> addUser(@RequestBody UserDTO userDTO) {
-        System.out.println(userDTO.toString());
-        return new ResponseEntity<>(userService.addUser(userDTO), HttpStatus.OK);
+    public ResponseEntity<?> addUser(@RequestBody UserDTO userDTO) {
+        try {
+            return new ResponseEntity<>(userService.addUser(userDTO), HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping(value="/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable("id") Integer id, @RequestBody UserDTO userDTO) {
-        return new ResponseEntity<>(userService.updateUser(id, userDTO), HttpStatus.OK);
+    public ResponseEntity<?> updateUser(@PathVariable("id") Integer id, @RequestBody UserDTO userDTO) {
+        try {
+            return new ResponseEntity<>(userService.updateUser(id, userDTO), HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.NOT_FOUND.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.CONFLICT.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    e.getMessage(),
+                    LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
+            );
+            return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
